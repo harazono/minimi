c321c8159a1caa8cad557adae2d654fdbd5e1f5b
harazono <harazono_yoritaka_17@stu-cbms.k.u-tokyo.ac.jp>
2019-03-26 14:40:05 +0900

    add .depend.mk

---
+++ src/.depend.mk
@@ -0,0 +1,4 @@
+count_kmer.o: count_kmer.cpp kmer_library.h stackdump.h cpas_debug.h \
+ cpas_tsv.h string_piece.h
+kmer_library_test.o: kmer_library_test.cpp kmer_library.h stackdump.h \
+ cpas_debug.h cpas_tsv.h string_piece.h
